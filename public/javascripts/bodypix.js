99a5406059c1c5c01a06bd38ef0b67dca8b4de9c
Abhishek Babu <abhideep0408@gmail.com>
2020-08-29 10:48:33 -0700

    Added body width calculation and display results on canvas

--- public/javascripts/bodypix.js
+++ public/javascripts/bodypix.js
@@ -8,9 +8,36 @@ let width = 720;
 let height = 540;
 let pixelHeight;
 
+// Body parts dictionary
+let parts = {
+  0: "left_face",
+  1: "right_face",
+  2: "left_upper_arm_front",
+  3: "left_upper_arm_back",
+  4: "right_upper_arm_front",
+  5: "right_upper_arm_back",
+  6: "left_lower_arm_front",
+  7: "left_lower_arm_back",
+  8: "right_lower_arm_front",
+  9: "right_lower_arm_back",
+  10: "left_hand",
+  11: "right_hand",
+  12: "torso_front",
+  13: "torso_back",
+  14: "left_upper_leg_front",
+  15: "left_upper_leg_back",
+  16: "right_upper_leg_front",
+  17: "right_upper_leg_back",
+  18: "left_lower_leg_front",
+  19: "left_lower_leg_back",
+  20: "right_lower_leg_front",
+  21: "right_lower_leg_back",
+  22: "left_foot",
+  23: "right_foot"
+}
+
 
 // Main
-
 async function main() {
   console.log("Loading...");
   camera = await loadCamera();
@@ -22,7 +49,6 @@ main();
 
 
 // Document elements helper methods
-
 async function loadCamera() {
   console.log("Loading camera...");
 
@@ -30,7 +56,7 @@ async function loadCamera() {
   cameraElement.width = width;
   cameraElement.height = height;
   cameraElement.setAttribute("style", "display: inline;")
-  document.body.appendChild(cameraElement);
+  document.getElementById('center').appendChild(cameraElement);
 
   const capture = await navigator.mediaDevices.getUserMedia({ video: true });
   cameraElement.srcObject = capture;
@@ -46,10 +72,12 @@ function createCanvas() {
   canvasElement.width = width;
   canvasElement.height = height;
   canvasElement.setAttribute("style", "display: none;");
-  document.body.appendChild(canvasElement);
+  document.getElementById('center').appendChild(canvasElement);
   return canvasElement;
 }
 
+
+// Button onclick functions
 function takePicture() {
   console.log("Taking picture");
 
@@ -71,33 +99,50 @@ function uploadPicture() {
   console.log("Uploading picture");
 
   camera.setAttribute("style", "display: none;");
+  canvas.setAttribute("style", "display: inline;");
 
   const img = document.getElementById('image');
+
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  }
+
   img.src = URL.createObjectURL(document.getElementById('upload').files[0]);
+
+  document.getElementById('uploadLabel').innerHTML = document.getElementById('upload').files[0].name.toString();
 
   bodyPixMain(img);
 }
 
-function examplePicture() {
+/*function examplePicture() {
   console.log("Using example picture...");
 
   camera.setAttribute("style", "display: none;");
+  canvas.setAttribute("style", "display: inline;");
 
   const img = document.getElementById('image');
+
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  }
+
   img.src = '/images/forest.jpeg';
 
   bodyPixMain(img);
-}
+}*/
 
 
 // BodyPix helper methods
-
 function bodyPixMain(img) {
-  loadModel()
-    .then(model =>
-      segment(model, img)
-        .then(segmentation =>
-          calculatePixelHeight(segmentation)));
+  const result = document.getElementById('result')
+  result.innerHTML = 'Processing...';
+  loadModel().then(model =>
+    segment(model, img).then(segmentations =>
+      calculatePixelHeight(segmentations)));
 }
 
 async function loadModel() {
@@ -109,44 +154,117 @@ async function loadModel() {
 async function segment(model, img) {
   console.log("Segmenting image...");
   const segmentation = await model.segmentPerson(img);
-  return segmentation;
+  const partSegmentation = await model.segmentPersonParts(img);
+  return [segmentation, partSegmentation];
 }
 
-function calculatePixelHeight(segmentation) {
-  console.log(segmentation);
+// Processing methods
+function calculatePixelHeight(segmentations) {
+  console.log(segmentations[0]);
+  console.log(segmentations[1]);
   console.log("Calculating pixel height...");
 
   var top = -1;
   var bottom = -1;
 
-  for (var i = 0; i < segmentation.data.length; i++) {
-    if (segmentation.data[i] !== 0) {
+  for (var i = 0; i < segmentations[0].data.length; i++) {
+    if (segmentations[0].data[i] !== 0) {
       top = i;
       break;
     }
   }
 
-  for (var j = segmentation.data.length - 1; j >= 0; j--) {
-    if (segmentation.data[j] !== 0) {
+  for (var j = segmentations[0].data.length - 1; j >= 0; j--) {
+    if (segmentations[0].data[j] !== 0) {
       bottom = j;
       break;
     }
   }
 
-  var topRow = top / segmentation.width;
-  var bottomRow = bottom / segmentation.width;
+  var topRow = top / segmentations[0].width;
+  var bottomRow = bottom / segmentations[0].width;
 
   pixelHeight = parseInt(bottomRow) - parseInt(topRow);
   console.log('Pixel height: ' + pixelHeight);
 
   const result = document.getElementById('result')
-  result.innerHTML = 'Pixel height: ' + pixelHeight;
-  result.innerHTML += '<br/>';
-  result.innerHTML += 'Picture width: ' + segmentation.width;
-  result.innerHTML += '<br/>';
-  result.innerHTML += 'Picture height: ' + segmentation.height;
+  result.innerHTML = 'Picture width: ' + segmentations[0].width;
+  result.innerHTML += '&emsp;';
+  result.innerHTML += 'Picture height: ' + segmentations[0].height;
+
+  var rightMost = calculatePixelWidth(segmentations[1]);
+  drawHeight(pixelHeight, topRow, bottomRow, rightMost);
 }
 
-function redirectRealtime() {
-  window.location.href += "realtime";
+function calculatePixelWidth(segmentation) {
+  console.log("Calculating pixel width");
+
+  var left = Number.POSITIVE_INFINITY;
+  var right = -1;
+
+  var rightMost = -1;
+
+  for (var i = 0; i < segmentation.data.length; i++) {
+    if (parts[segmentation.data[i]] === "torso_front") {
+      var xCoordinate = i % segmentation.width;
+      left = Math.min(left, xCoordinate);
+      right = Math.max(right, xCoordinate);
+    }
+    if (segmentation.data[i] !== -1) {
+      var xCoordinate = i % segmentation.width;
+      rightMost = Math.max(rightMost, xCoordinate);
+    }
+  }
+
+  pixelWidth = parseInt(right) - parseInt(left);
+  console.log('Pixel width: ' + pixelWidth);
+
+  drawWidth(pixelWidth, left, right);
+
+  return rightMost;
 }
+
+// Drawing methods
+function drawHeight(height, yTop, yBottom, rightMost) {
+  x = Math.min(parseInt(0.9 * canvas.width), rightMost + 20);
+
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'red';
+
+  ctx.font = '32px serif'
+  yText = parseInt((yTop + yBottom) / 2);
+  xText = x + 10;
+  maxWidth = parseInt(0.1 * canvas.width) - 10;
+  ctx.fillText(height.toString(), xText, yText, maxWidth);
+
+  ctx.beginPath();
+  ctx.moveTo(x, yTop);
+  ctx.lineTo(x, yBottom);
+  ctx.lineWidth = 5;
+  ctx.stroke();
+}
+
+function drawWidth(width, xLeft, xRight) {
+  y = parseInt(0.9 * canvas.height);
+
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'red';
+
+  ctx.font = '32px serif';
+  xText = parseInt((xLeft + xRight) / 2) - 15;
+  if (width >= 100) {
+    xText -= 15;
+  }
+  yText = y + 40;
+  ctx.fillText(width.toString(), xText, yText);
+
+  ctx.beginPath();
+  ctx.moveTo(xLeft, y);
+  ctx.lineTo(xRight, y);
+  ctx.lineWidth = 5;
+  ctx.stroke();
+}
+
+/*function redirectRealtime() {
+  window.location.href += "realtime";
+}*/
