56eab5cb21136b08c99e2fa2fae39adf99ca4928
Abhishek Babu <abhideep0408@gmail.com>
2021-01-01 19:47:48 -0800

    Added metrics graphs

--- public/javascripts/bodypix.js
+++ public/javascripts/bodypix.js
@@ -583,38 +583,3 @@ function showFive() {
     document.getElementById("btn1").className = document.getElementById("btn1").className.replace( /(?:^|\s)active(?!\S)/g , '' )
   }
 }
-
-// Replace with your view ID.
-var VIEW_ID = '235009059';
-
-// Query the API and print the results to the page.
-function queryReports() {
-  gapi.client.request({
-    path: '/v4/reports:batchGet',
-    root: 'https://analyticsreporting.googleapis.com/',
-    method: 'POST',
-    body: {
-      reportRequests: [
-        {
-          viewId: VIEW_ID,
-          dateRanges: [
-            {
-              startDate: '7daysAgo',
-              endDate: 'today'
-            }
-          ],
-          metrics: [
-            {
-              expression: 'ga:sessions'
-            }
-          ]
-        }
-      ]
-    }
-  }).then(displayResults, console.error.bind(console));
-}
-
-function displayResults(response) {
-  var formattedJson = JSON.stringify(response.result, null, 2);
-  document.getElementById('query-output').value = formattedJson;
-}
